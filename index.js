26868fb22840a09a2693dd350a431ab342cda921
oguzhndlc <oguzdalg@gmail.com>
2025-07-07 20:20:14 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/veri', (req, res) => {
+  res.json([{ isim: "oguzhan", skor: 3 }]);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Sunucu çalışıyor: ${port}`));
