b4369b3bcf3284db6ad5bf335ed0bcaf7a3304e4
oguzhndlc <oguzdalg@gmail.com>
2025-07-07 20:45:53 +0300

    Update index.php

--- index (1).php
+++ index (1).php
@@ -1,5 +1,5 @@
 <?php
 // Powered by Site.pro
 if (function_exists('ini_set')) @ini_set('opcache.enable', '0');
-include dirname(__FILE__).'/sitepro/index.php';
+include dirname(__FILE__).'/main/index.php';
 ?>
